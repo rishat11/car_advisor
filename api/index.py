8a7cdc04b189c0f3d02bbd3e81df76dc1bcaa9da
rishat11 <r.sadickov2010@yandex.ru>
2026-01-26 02:56:00 +0300

    vercel changes

--- api/index.py
+++ api/index.py
@@ -1,7 +1,11 @@
 import os
 import sys
+from mangum import Mangum
 from app.main import app
 
-# Для Vercel нужно экспортировать приложение как 'app'
-# Это позволяет Vercel правильно распознать ASGI приложение
-handler = app
+# Для Vercel нужно правильно обернуть FastAPI приложение в Mangum
+# Это позволяет Vercel распознать наше приложение как ASGI-совместимое
+handler = Mangum(app, lifespan="off")
+
+# Также убедимся, что в serverless среде не используется lifespan
+# потому что подключение к БД будет происходить при каждом вызове
